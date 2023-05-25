67f402ce17b1b45b989eb7ca91fe84a62ab8de59
wiigny <wigny_36@hotmail.com>
2023-05-25 14:34:16 -0300

    fix: remove console log

--- back/src/services/clients/get.service.ts
+++ back/src/services/clients/get.service.ts
@@ -38,8 +38,6 @@ export const retrieveClientService = async (
 ): Promise<TReturnClientCreated> => {
   const findClient = clientFounded;
 
-  console.log(findClient);
-
   const clientParsed = returnClientCreatedSchema.parse(findClient);
 
   return clientParsed;
